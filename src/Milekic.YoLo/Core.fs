6b4e040e842d7f4a1dc2a3c4c808db4d25a89dfd
Nikola Milekic <nikolamilekic@gmail.com>
2018-04-28 12:41:11 +0200

    Add atomicUpdateQuery

--- src/Milekic.YoLo/Core.fs
+++ src/Milekic.YoLo/Core.fs
@@ -1,6 +1,15 @@
 [<AutoOpen>]
 module Milekic.YoLo.Core
 
+open System.Threading
+
 let curry f a b = f(a, b)
 let uncurry f (a, b) = f a b
 let flip f a b = f b a
+let rec atomicUpdateQuery state update =
+    let oldState = !state
+    let result, newState = update oldState
+    let ok = Interlocked.CompareExchange<_>(state, newState, oldState)
+             |> LanguagePrimitives.PhysicalEquality oldState
+    if ok then result, newState else atomicUpdateQuery state update
+let atomicUpdateQueryResult s u = (s, u) ||> atomicUpdateQuery |> fst
