52556cad8a061c7ce5c90af282d6f8b10e33acdd
Nikola Milekic <nikolamilekic@gmail.com>
2019-02-01 20:26:04 +0100

    Add nameOf and instanceOf operators

--- src/Milekic.YoLo/Core.fs
+++ src/Milekic.YoLo/Core.fs
@@ -2,6 +2,7 @@
 module Milekic.YoLo.Core
 
 open System.Threading
+open Microsoft.FSharp.Quotations
 
 let curry f a b = f(a, b)
 let uncurry f (a, b) = f a b
@@ -13,3 +14,10 @@ let rec atomicUpdateQuery state update =
              |> LanguagePrimitives.PhysicalEquality oldState
     if ok then result, newState else atomicUpdateQuery state update
 let atomicUpdateQueryResult s u = (s, u) ||> atomicUpdateQuery |> fst
+
+let nameOf = function
+    | Patterns.PropertyGet(_, propertyInfo, _) -> propertyInfo.Name
+    | Patterns.FieldGet(_, fieldInfo) -> fieldInfo.Name
+    | _ -> failwith "Unsupported quotation was passed to nameof"
+
+let instanceOf<'T> : 'T = failwith "instanceOf operator should only be used in expressions and should never actually be called"
