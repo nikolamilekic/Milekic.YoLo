544b625c54e21eec9ce44e8fd9ca7aaa52e5fc94
Nikola Milekic <nikolamilekic@gmail.com>
2019-03-15 18:04:02 +0100

    Expand Result operators and builder

--- src/Milekic.YoLo/Result.fs
+++ src/Milekic.YoLo/Result.fs
@@ -1,6 +1,7 @@
 module Milekic.YoLo.Result
 
 open Result
+open System
 
 let either ok error = function | Ok x -> ok x | Error x -> error x
 let liftChoice = function | Choice1Of2 x -> Ok x
@@ -16,6 +17,9 @@ module Operators =
     let inline (>>=) e f = bind f e
     let inline (>=>) f1 f2 e = f1 e >>= f2
     let inline (>>-) e f = map f e
+    let inline (>>-!) x f = Result.mapError f x
+    let inline (>>-!.) x error = Result.mapError (fun _ -> error) x
+    let inline (>>-.) x value = Result.map (fun _ -> value) x
 
 open Operators
 
@@ -30,3 +34,10 @@ type Builder() =
     member __.Bind(e, f) = bind f e
     member __.Return x = Ok x
     member __.ReturnFrom x = x
+    member __.Zero () = Ok ()
+    member __.Delay f = f
+    member __.Run f = f()
+    member __.TryWith(f, handler) = try f() with e -> handler e
+    member __.TryFinally(f, compensation) = try f() finally compensation()
+    member this.Using(d : #IDisposable, f) =
+        this.TryFinally((fun () -> f d), d.Dispose)
