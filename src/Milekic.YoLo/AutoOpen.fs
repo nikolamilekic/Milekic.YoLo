d1e1a9c6d2076541e64b3b4ee435b89b1a5fe082
Nikola Milekic <nikolamilekic@gmail.com>
2019-07-14 17:55:14 +0200

    Add option builder and traverse + sequence methods

--- src/Milekic.YoLo/AutoOpen.fs
+++ src/Milekic.YoLo/AutoOpen.fs
@@ -4,3 +4,4 @@ module Milekic.YoLo.AutoOpen
 let result = Result.Builder()
 let update = Update.Builder()
 let updateResult = UpdateResult.Builder()
+let option = Option.Builder()
--- src/Milekic.YoLo/Option.fs
+++ src/Milekic.YoLo/Option.fs
@@ -1,5 +1,7 @@
 namespace Milekic.YoLo
 
+open System
+
 module Seq =
     let onlySome xs = seq {
         for x in xs do match x with | Some v -> yield v | None -> ()
@@ -11,3 +13,33 @@ module List =
             (fun x xs -> match x with | None -> xs | Some v -> v::xs )
             list
             []
+
+open Option
+module Option =
+    let traverse f (source : _ seq) =
+        use enumerator = source.GetEnumerator()
+        let rec inner state =
+            if enumerator.MoveNext() = false then Some (state |> List.rev) else
+            match f enumerator.Current with
+            | None -> None
+            | Some x -> inner (x::state)
+        inner []
+    let sequence source = traverse id source
+
+    module Operators =
+        let inline (>>=) e f = bind f e
+        let inline (>=>) f1 f2 e = f1 e >>= f2
+        let inline (>>-) e f = map f e
+        let inline (>>-.) x value = map (fun _ -> value) x
+
+    type Builder() =
+        member __.Bind(e, f) = bind f e
+        member __.Return x = Some x
+        member __.ReturnFrom x = x
+        member __.Zero () = Some ()
+        member __.Delay f = f
+        member __.Run f = f()
+        member __.TryWith(f, handler) = try f() with e -> handler e
+        member __.TryFinally(f, compensation) = try f() finally compensation()
+        member this.Using(d : #IDisposable, f) =
+            this.TryFinally((fun () -> f d), d.Dispose)
