6942982b0febb4d3956373888266c3321ecfdad4
Nikola Milekic <nikolamilekic@hey.com>
2022-01-26 19:46:45 +0100

    Remove obsolete code

--- src/Milekic.YoLo/Core.fs
+++ src/Milekic.YoLo/Core.fs
@@ -1,32 +1,17 @@
 [<AutoOpen>]
 module Milekic.YoLo.Core
 
-open System
 open System.Text.RegularExpressions
 open System.Threading
 open System.Collections.Generic
-open Microsoft.FSharp.Quotations.Patterns
 
-[<Obsolete("Use FSharpPlus instead")>]
-let curry f a b = f(a, b)
-[<Obsolete("Use FSharpPlus instead")>]
-let uncurry f (a, b) = f a b
-[<Obsolete("Use FSharpPlus instead")>]
-let flip f a b = f b a
-let rec atomicUpdateQuery state update =
-    let oldState = !state
+let rec atomicUpdateQuery (state : 'a ref) update =
+    let oldState = state.Value
     let result, newState = update oldState
     let ok = Interlocked.CompareExchange<_>(state, newState, oldState)
              |> LanguagePrimitives.PhysicalEquality oldState
     if ok then result, newState else atomicUpdateQuery state update
 let atomicUpdateQueryResult s u = (s, u) ||> atomicUpdateQuery |> fst
-
-let nameOf = function
-    | PropertyGet(_, propertyInfo, _) -> propertyInfo.Name
-    | FieldGet(_, fieldInfo) -> fieldInfo.Name
-    | Lambda (_, NewUnionCase (info, _)) -> info.Name
-    | Lambda (_, Call (_, method, _)) -> method.Name
-    | e -> failwithf "Unsupported quotation was passed to nameOf %A" e
 
 let instanceOf<'T> : 'T = failwith "instanceOf operator should only be used in expressions and should never actually be called"
 
--- src/Milekic.YoLo/Result.fs
+++ src/Milekic.YoLo/Result.fs
@@ -1,55 +1,6 @@
+[<RequireQualifiedAccess>]
 module Milekic.YoLo.Result
 
-open Result
-open System
-
-#nowarn "44"
-
-[<Obsolete("Use FSharpPlus instead")>]
-let either ok error = function | Ok x -> ok x | Error x -> error x
-[<Obsolete("Use FSharpPlus instead")>]
-let liftOption error = function | Some x -> Ok x
-                                | None -> Error error
-[<Obsolete("Use FSharpPlus instead")>]
-let liftChoice = function | Choice1Of2 x -> Ok x
-                          | Choice2Of2 error -> Error error
-[<Obsolete("Use FSharpPlus instead")>]
-let toChoice e = e |> either Choice1Of2 Choice2Of2
-let isOk e = either (fun _ -> true) (fun _ -> false) e
-let isError e = isOk e |> not
-[<Obsolete("Use FSharpPlus instead")>]
-let defaultWith f = either id f
-[<Obsolete("Use FSharpPlus instead")>]
-let defaultValue x = defaultWith (fun _ -> x)
-let failOnError message = defaultWith <| fun _ -> failwith message
-let traverse f (source : _ seq) =
-    use enumerator = source.GetEnumerator()
-    let rec inner state =
-        if enumerator.MoveNext() = false then Ok (state |> List.rev) else
-        match f enumerator.Current with
-        | Error x -> Error x
-        | Ok x -> inner (x::state)
-    inner []
-let sequence source = traverse id source
-
-[<Obsolete("Use FSharpPlus instead")>]
-module Operators =
-    let inline (>>=) e f = bind f e
-    let inline (>=>) f1 f2 e = f1 e >>= f2
-    let inline (>>-) e f = map f e
-    let inline (>>-!) x f = Result.mapError f x
-    let inline (>>-!.) x error = Result.mapError (fun _ -> error) x
-    let inline (>>-.) x value = Result.map (fun _ -> value) x
-
-[<Obsolete("Use FSharpPlus instead")>]
-type Builder() =
-    member __.Bind(e, f) = bind f e
-    member __.Return x = Ok x
-    member __.ReturnFrom x = x
-    member __.Zero () = Ok ()
-    member __.Delay f = f
-    member __.Run f = f()
-    member __.TryWith(f, handler) = try f() with e -> handler e
-    member __.TryFinally(f, compensation) = try f() finally compensation()
-    member this.Using(d : #IDisposable, f) =
-        this.TryFinally((fun () -> f d), d.Dispose)
+let isOk = function | Ok _ -> true | Error _ -> false
+let isError = function | Ok _ -> false | Error _ -> true
+let failOnError message = Option.defaultWith <| fun _ -> failwith message
