f2037e20d093a82bddf4ed68f57df764c7c45ce8
Nikola Milekic <nikolamilekic@hey.com>
2022-01-28 23:22:20 +0100

    Add product description metadata

--- src/Milekic.YoLo/Metadata.fs
+++ src/Milekic.YoLo/Metadata.fs
@@ -1,12 +1,43 @@
+[<RequireQualifiedAccess>]
 module Milekic.YoLo.Metadata
 
+open System
 open System.Reflection
 
-let getCallingAssemblyInformationalVersion() =
-    let informationalVersion =
-        Assembly
-            .GetCallingAssembly()
-            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-            .InformationalVersion
+let entryAssembly = lazy Assembly.GetEntryAssembly()
 
-    informationalVersion.Substring(0, informationalVersion.IndexOf("+") + 8)
+let entryAssemblyInformationalVersion =
+    lazy
+    entryAssembly.Value.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+    |> Option.ofObj
+
+let entryAssemblyProduct =
+    lazy
+    entryAssembly.Value.GetCustomAttribute<AssemblyProductAttribute>()
+    |> Option.ofObj
+
+let entryAssemblyReleaseDate =
+    lazy
+    entryAssembly.Value.GetCustomAttributes<AssemblyMetadataAttribute>()
+    |> Seq.tryFind(fun attribute -> attribute.Key = "ReleaseDate")
+
+let productDescription =
+    lazy
+    entryAssemblyProduct.Value
+    |> Option.bind (fun p ->
+        entryAssemblyInformationalVersion.Value |> Option.map (fun v -> (p, v)))
+    |> Option.map (fun (productAttribute, versionAttribute) ->
+        let product = productAttribute.Product
+        let version =
+            let v = versionAttribute.InformationalVersion
+            if v.Contains("+") then v.Substring(0, v.IndexOf("+") + 8) else v
+        let dateMaybe =
+            entryAssemblyReleaseDate.Value
+            |> Option.bind (fun dateAttribute ->
+                match DateTimeOffset.TryParse(dateAttribute.Value) with
+                | true, date -> Some (date.ToString("yyyy-MM-dd"))
+                | _ -> None)
+        match dateMaybe with
+        | Some dateAsString -> $"{product} Version: {version} ({dateAsString})"
+        | None -> $"{product} Version: {version}")
+    |> Option.defaultValue ""
--- src/Milekic.YoLo/Result.fs
+++ src/Milekic.YoLo/Result.fs
@@ -3,4 +3,4 @@ module Milekic.YoLo.Result
 
 let isOk = function | Ok _ -> true | Error _ -> false
 let isError = function | Ok _ -> false | Error _ -> true
-let failOnError message = Option.defaultWith <| fun _ -> failwith message
+let failOnError message = function | Ok x -> x | Error _ -> failwith message
